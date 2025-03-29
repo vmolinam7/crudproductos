202fb80bf34910bb957d6dd832e07b1b0b984e5e
vmolinam7 <victor.molina2017@uteq.edu.ec>
2025-03-28 22:01:11 -0500

    cambiando el setting

--- crudproducto/settings.py
+++ crudproducto/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$9)d99x8epo_=-)k2+p4%-3$89r4@wf+=g(p#3oyvbdk@igg&8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
